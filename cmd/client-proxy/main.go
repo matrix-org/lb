f4116b5e1d75deb07d8faa000c44564618355cb0
Kegan Dougal <kegan@matrix.org>
2021-09-16 12:21:52 +0100

    Upgrade mobile deps

--- cmd/client-proxy/main.go
+++ cmd/client-proxy/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"flag"
 
+	"github.com/matrix-org/lb/mobile"
+)
+
+var (
+	httpBindAddr   = flag.String("http-bind-addr", ":8008", "The HTTP listening port for the server")
+	homeserverAddr = flag.String("homeserver", "", "The homeserver to forward inbound requests to e.g coaps://localhost:8008")
+)
+
+func main() {
+	mobile.SendRequest("", "", "", "")
 }
